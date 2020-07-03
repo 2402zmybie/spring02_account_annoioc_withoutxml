8cb35240ed52382cdaf95e914c354cac601ca1c3
Lc <641229781@qq.com>
2020-07-03 14:40:22 +0800

    注解配置类实现全注解的spring

---
+++ src/main/java/conf/JdbcConfig.java
@@ -0,0 +1,47 @@
+package conf;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.apache.commons.dbutils.QueryRunner;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+
+/**
+ * 用于JDBC的配置类, 要被主配置类导入
+ */
+public class JdbcConfig {
+
+    @Value("${jdbc.driver}")
+    private String driver;
+    @Value("${jdbc.url}")
+    private String url;
+    @Value("${jdbc.username}")
+    private String user;
+    @Value("${jdbc.password}")
+    private String password;
+
+
+    @Bean(name = "runner")
+    @Scope("prototype")
+    public QueryRunner createQueryRunner(DataSource ds) {
+        return new QueryRunner(ds);
+    }
+
+    @Bean(name = "ds")
+    public ComboPooledDataSource createComboPooledDataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass(driver);
+            dataSource.setJdbcUrl(url);
+            dataSource.setUser(user);
+            dataSource.setPassword(password);
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+        return dataSource;
+    }
+}
